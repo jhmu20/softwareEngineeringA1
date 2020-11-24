8b093019287221b99b65aa92d512dbce7c5dff68
jhmu20 <38563502+jhmu20@users.noreply.github.com>
2020-11-24 21:46:15 +0000

    test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class test {
+    ArrayList<Student> student;
+    ArrayList<Module> module;
+    //ArrayList<Course> course;
+    Course course;
+    Date date;
+    Student testStudent = new Student("Jarlath Healy", 21, 17419602, course, new ArrayList<Module>(), date);
+    Module testModule = new Module("Maths", 246, new ArrayList<Student>(), new ArrayList<Course>());
+    Course testCourse = new Course("BST", new ArrayList<Student>(), new ArrayList<Module>(), new LocalDate(2018-06-07), new LocalDate(2019-05-04) );
+
+
+    
+    @Test
+    public void usernameReturnsNamePlusAge() {
+        Assert.assertEquals("jarlathhealy21", testStudent.getUserName(), "" + "Correct, Test Passed");
+    }
+
+    @Test
+    public void testAddCourse() {
+        testStudent.addCourse(testCourse);
+    }
+
+    
 }
+
